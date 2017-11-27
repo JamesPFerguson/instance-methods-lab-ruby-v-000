e708087f6cd20248453242db4eef9e612a8cb99a
James Ferguson <ncjamesferguson@gmail.com>
2017-11-27 04:45:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,16 @@ class Dog
     puts "The Dog is sitting"
   end
 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name
